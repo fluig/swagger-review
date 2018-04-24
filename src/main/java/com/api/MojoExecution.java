de9cfb411ba04e6f037a8427a798fdac5d4b0bc5
JV01\rafael.savaris <rafael.savaris@fluig.com>
2018-04-24 15:05:01 -0300

    include reflection to list Rules include treatment to add ignoreList for rules

--- src/main/java/com/api/MojoExecution.java
+++ src/main/java/com/api/MojoExecution.java
@@ -7,15 +7,23 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
+import java.util.ArrayList;
+
 @Mojo(name = "review", requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class MojoExecution extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true)
     private MavenProject project;
 
+    @Parameter(readonly = true)
+    private ArrayList<String> ignoreRules = null;
+
     @Override
     public void execute() throws MojoExecutionException {
-        Utils.executeValidation(project.getBasedir().toPath());
+
+        System.out.println("IGNORE: " + ignoreRules);
+
+        Utils.executeValidation(project.getBasedir().toPath(), ignoreRules);
     }
 
 }
