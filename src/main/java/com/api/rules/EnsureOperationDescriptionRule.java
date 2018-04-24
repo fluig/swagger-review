a28b01b5951c927a579876413eb95e4636e8df02
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-24 11:31:40 -0300

    Validate if operation description exists

--- src/main/java/com/api/rules/EnsureOperationDescriptionRule.java
+++ src/main/java/com/api/rules/EnsureOperationDescriptionRule.java
@@ -2,9 +2,15 @@ package com.api.rules;
 
 import com.api.factory.SwaggerRuleFailure;
 import com.api.factory.SwaggerRuleType;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
+import org.codehaus.plexus.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class EnsureOperationDescriptionRule implements SwaggerRule {
 
@@ -28,6 +34,25 @@ public class EnsureOperationDescriptionRule implements SwaggerRule {
 
     @Override
     public List<SwaggerRuleFailure> execute(Swagger swagger) {
-        return null;
+
+        ArrayList<SwaggerRuleFailure> failures = new ArrayList<>();
+
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+
+            for (Map.Entry<HttpMethod, Operation> operationEntry : pathEntry.getValue().getOperationMap().entrySet()) {
+
+                if (StringUtils.isEmpty(operationEntry.getValue().getDescription())) {
+                    SwaggerRuleFailure failure = new SwaggerRuleFailure(
+                            getName(),
+                            String.format(MESSAGE, operationEntry.getKey().name() + " " + pathEntry.getKey()),
+                            getType()
+                    );
+
+                    failures.add(failure);
+                }
+            }
+        }
+
+        return failures;
     }
 }
