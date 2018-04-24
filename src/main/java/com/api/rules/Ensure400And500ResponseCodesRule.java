a1b458cf306af5745024bfb92ffb85db7d23d5d8
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-24 15:24:33 -0300

    Validate whether each path has a 400 and 500 error response

--- src/main/java/com/api/rules/Ensure400And500ResponseCodesRule.java
+++ src/main/java/com/api/rules/Ensure400And500ResponseCodesRule.java
@@ -2,14 +2,21 @@ package com.api.rules;
 
 import com.api.factory.SwaggerRuleFailure;
 import com.api.factory.SwaggerRuleType;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
+import io.swagger.models.Response;
 import io.swagger.models.Swagger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public final class Ensure400And500ResponseCodesRule implements SwaggerRule {
 
     private static final String RULENAME = "RULE0005";
-    private static final String MESSAGE = "O path {path} não mapea os códigos de retorno 400 ou 500.";
+    private static final String MESSAGE = "O path %s não mapea os códigos de retorno 400 ou 500.";
 
     @Override
     public String getName() {
@@ -28,6 +35,32 @@ public final class Ensure400And500ResponseCodesRule implements SwaggerRule {
 
     @Override
     public List<SwaggerRuleFailure> execute(Swagger swagger) {
-        return null;
+
+        List<String> expectedResponses = Arrays.asList("400", "500");
+
+        ArrayList<SwaggerRuleFailure> failures = new ArrayList<>();
+
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+
+            for (Map.Entry<HttpMethod, Operation> operationEntry : pathEntry.getValue().getOperationMap().entrySet()) {
+
+                Map<String, Response> responsesEntry = operationEntry.getValue().getResponses();
+
+                for (String responseCode: expectedResponses) {
+
+                    if (!responsesEntry.containsKey(responseCode)) {
+                        failures.add(new SwaggerRuleFailure(
+                                getName(),
+                                String.format(MESSAGE, operationEntry.getKey().name() + " " + pathEntry.getKey()),
+                                getType()
+                        ));
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        return failures;
     }
 }
