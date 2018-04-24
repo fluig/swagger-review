5dece7cccedc8afa1e9faa2d3e84cf0001702943
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-24 15:55:19 -0300

    Create 404 response for path params rule class

---
+++ src/main/java/com/api/rules/EnsurePathParam404ResponseRule.java
@@ -0,0 +1,30 @@
+package com.api.rules;
+
+import com.api.factory.SwaggerRuleFailure;
+import com.api.factory.SwaggerRuleType;
+import io.swagger.models.Swagger;
+
+import java.util.List;
+
+public final class EnsurePathParam404ResponseRule implements SwaggerRule {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public SwaggerRuleType getType() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public List<SwaggerRuleFailure> execute(Swagger swagger) {
+        return null;
+    }
+}
