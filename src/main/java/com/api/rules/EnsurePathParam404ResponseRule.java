af0f88d3f63ee9487f6db236c8a2604e8f07e48d
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-24 16:56:09 -0300

    Validade whether paths with parameters map 404 responses

--- src/main/java/com/api/rules/EnsurePathParam404ResponseRule.java
+++ src/main/java/com/api/rules/EnsurePathParam404ResponseRule.java
@@ -2,9 +2,15 @@ package com.api.rules;
 
 import com.api.factory.SwaggerRuleFailure;
 import com.api.factory.SwaggerRuleType;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public final class EnsurePathParam404ResponseRule implements SwaggerRule {
 
@@ -28,6 +34,35 @@ public final class EnsurePathParam404ResponseRule implements SwaggerRule {
 
     @Override
     public List<SwaggerRuleFailure> execute(Swagger swagger) {
-        return null;
+
+        final String expectedParameterLocation = "path";
+        final String expectedResponseCode = "404";
+
+        ArrayList<SwaggerRuleFailure> failures = new ArrayList<>();
+
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+
+            for (Map.Entry<HttpMethod, Operation> operationEntry : pathEntry.getValue().getOperationMap().entrySet()) {
+
+                for (Parameter parameter : operationEntry.getValue().getParameters()) {
+
+                    if (parameter.getIn().equals(expectedParameterLocation) &&
+                            !operationEntry.getValue().getResponses().containsKey(expectedResponseCode)) {
+
+                        SwaggerRuleFailure failure = new SwaggerRuleFailure(
+                                getName(),
+                                String.format(MESSAGE, operationEntry.getKey().name() + " " + pathEntry.getKey()),
+                                getType()
+                        );
+
+                        failures.add(failure);
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        return failures;
     }
 }
