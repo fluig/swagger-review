644ee695c5e378e65f76c8e364be34eef565e494
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-24 15:57:23 -0300

    Return the appropriate information about the new rule

--- src/main/java/com/api/rules/EnsurePathParam404ResponseRule.java
+++ src/main/java/com/api/rules/EnsurePathParam404ResponseRule.java
@@ -8,19 +8,22 @@ import java.util.List;
 
 public final class EnsurePathParam404ResponseRule implements SwaggerRule {
 
+    private static final String RULENAME = "RULE0003";
+    private static final String MESSAGE = "O path da api %s possui um placeholder que identifica uma entidade e portanto deve mapear o código de erro 404.";
+
     @Override
     public String getName() {
-        return null;
+        return RULENAME;
     }
 
     @Override
     public SwaggerRuleType getType() {
-        return null;
+        return SwaggerRuleType.ERROR;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return "Paths que identificam uma entidade pelo id/code devem retornar 404.";
     }
 
     @Override
