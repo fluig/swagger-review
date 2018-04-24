083ef43940e0d8355f724bf9e583b6653d1f1519
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-24 15:01:16 -0300

    Return the appropriate information about the new rule

--- src/main/java/com/api/rules/Ensure400And500ResponsesRule.java
+++ src/main/java/com/api/rules/Ensure400And500ResponsesRule.java
@@ -8,19 +8,22 @@ import java.util.List;
 
 public final class Ensure400And500ResponsesRule implements SwaggerRule {
 
+    private static final String RULENAME = "RULE0005";
+    private static final String MESSAGE = "O path {path} não mapea os códigos de retorno 400 ou 500.";
+
     @Override
     public String getName() {
-        return null;
+        return RULENAME;
     }
 
     @Override
     public SwaggerRuleType getType() {
-        return null;
+        return SwaggerRuleType.ERROR;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return "Todas as APIs devem mapear os códigos de resposta 400 e 500.";
     }
 
     @Override
