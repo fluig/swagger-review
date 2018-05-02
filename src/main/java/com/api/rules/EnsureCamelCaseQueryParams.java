0ffc8f2b4c90c9f3aad569d788143f5f7f9a46b6
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-05-02 11:15:25 -0300

    Validate whether query params obey the camel case pattern

--- src/main/java/com/api/rules/EnsureCamelCaseQueryParams.java
+++ src/main/java/com/api/rules/EnsureCamelCaseQueryParams.java
@@ -2,9 +2,17 @@ package com.api.rules;
 
 import com.api.factory.EnumRule;
 import com.api.factory.SwaggerRuleFailure;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class EnsureCamelCaseQueryParams extends SwaggerRule {
 
@@ -14,6 +22,36 @@ public final class EnsureCamelCaseQueryParams extends SwaggerRule {
 
     @Override
     public List<SwaggerRuleFailure> execute(Swagger swagger) {
-        return null;
+
+        final String expectedParameterType = "query";
+        final String camelCasePattern = "^[a-z]+([A-Z]([a-z0-9])+)*";
+
+        ArrayList<SwaggerRuleFailure> failures = new ArrayList<>();
+
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+
+            for (Map.Entry<HttpMethod, Operation> operationEntry : pathEntry.getValue().getOperationMap().entrySet()) {
+
+                for (Parameter parameter : operationEntry.getValue().getParameters()) {
+
+                    if (!parameter.getIn().equals(expectedParameterType)) {
+                        continue;
+                    }
+
+                    Pattern pattern = Pattern.compile(camelCasePattern);
+                    Matcher matcher = pattern.matcher(parameter.getName());
+
+                    if (!matcher.matches()) {
+                        failures.add(new SwaggerRuleFailure(
+                                getName(),
+                                String.format(getMessage(), operationEntry.getKey().name() + " " + pathEntry.getKey()),
+                                getSwaggerRuleType()
+                        ));
+                    }
+                }
+            }
+        }
+
+        return failures;
     }
 }
