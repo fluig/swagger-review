a2b96e525870676f61e0c4d6f3309684f81e2f5d
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-05-04 15:15:36 -0300

    Ensure all operations have at least one tag

--- src/main/java/com/api/rules/EnsureAtLeastOneTagPerOperationRule.java
+++ src/main/java/com/api/rules/EnsureAtLeastOneTagPerOperationRule.java
@@ -2,9 +2,14 @@ package com.api.rules;
 
 import com.api.factory.EnumRule;
 import com.api.factory.SwaggerRuleFailure;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public final class EnsureAtLeastOneTagPerOperationRule extends SwaggerRule {
 
@@ -13,7 +18,24 @@ public final class EnsureAtLeastOneTagPerOperationRule extends SwaggerRule {
     }
 
     @Override
-    public List<SwaggerRuleFailure> execute(Swagger swagger) throws Exception {
-        return null;
+    public List<SwaggerRuleFailure> execute(Swagger swagger) {
+
+        ArrayList<SwaggerRuleFailure> failures = new ArrayList<>();
+
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+
+            for (Map.Entry<HttpMethod, Operation> operationEntry : pathEntry.getValue().getOperationMap().entrySet()) {
+
+                if (operationEntry.getValue().getTags() == null || operationEntry.getValue().getTags().size() < 1) {
+                    failures.add(new SwaggerRuleFailure(
+                            getName(),
+                            String.format(getMessage(), operationEntry.getKey().name() + " " + pathEntry.getKey()),
+                            getSwaggerRuleType()
+                    ));
+                }
+            }
+        }
+
+        return failures;
     }
 }
