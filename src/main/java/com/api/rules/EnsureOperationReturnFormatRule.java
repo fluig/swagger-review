d430c8f506792e81085b46a6d658649be79bd9ea
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-05-04 15:48:46 -0300

    Validate whether all operations list their return format

--- src/main/java/com/api/rules/EnsureOperationReturnFormatRule.java
+++ src/main/java/com/api/rules/EnsureOperationReturnFormatRule.java
@@ -2,9 +2,14 @@ package com.api.rules;
 
 import com.api.factory.EnumRule;
 import com.api.factory.SwaggerRuleFailure;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public final class EnsureOperationReturnFormatRule extends SwaggerRule {
 
@@ -14,6 +19,23 @@ public final class EnsureOperationReturnFormatRule extends SwaggerRule {
 
     @Override
     public List<SwaggerRuleFailure> execute(Swagger swagger) throws Exception {
-        return null;
+
+        ArrayList<SwaggerRuleFailure> failures = new ArrayList<>();
+
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+
+            for (Map.Entry<HttpMethod, Operation> operationEntry : pathEntry.getValue().getOperationMap().entrySet()) {
+
+                if (operationEntry.getValue().getProduces() == null || operationEntry.getValue().getProduces().size() < 1) {
+                    failures.add(new SwaggerRuleFailure(
+                            getName(),
+                            String.format(getMessage(), operationEntry.getKey().name() + " " + pathEntry.getKey()),
+                            getSwaggerRuleType()
+                    ));
+                }
+            }
+        }
+
+        return failures;
     }
 }
